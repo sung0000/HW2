a3fce29cc910a8b328f5ae816583c0129c2742b4
sung0000 <jpcsung@gmail.com>
2020-03-31 21:31:29 +0800

    Add main.cpp

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ char table[10] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F};
 int sample = 1200;
 float ADCdata[1200];
 float f, t;
+float j ;
 int f2, n, flag;
 
 int main(){
@@ -33,10 +34,12 @@ int main(){
     }
     f = 1000/t;
     f2 = round(f);
+    pc.printf("%1.3f\r\n", f);
+    pc.printf("%1.3f\r\n", t);
     while(1){
-        for(float i=0; i<2; i+=0.025){
-            Aout = 0.5 + 0.5*sin(i*3.14159);
-            wait(1./(80*f));
+        for(j=0; j<2; j+=0.05){
+            Aout = 0.5 + 0.5*sin(j*3.14159);
+            wait(1./(40*f));
         }
 
         if (Switch == 1){
